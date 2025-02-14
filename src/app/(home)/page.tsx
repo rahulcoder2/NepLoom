69790bc3626fcc0dc486ef410b69afd02c8d9e32
rahulcoder2 <rahulchaudharycsit077@gmail.com>
2025-02-14 12:50:52 +0545

    add : auth

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,18 +1,8 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <h1 className="h1 font-inter">Hello, World!</h1>
-      <p className="body-2 ">pop</p>
-      <Image
-        src="/globe.svg"
-        alt="Vercel Logo"
-        width={72}
-        height={16}
-        priority
-        className="h-auto w-16 text-black"
-      />
     </div>
   );
 }
--- src/components/shared/container.tsx
+++ src/components/shared/container.tsx
@@ -9,7 +9,10 @@ const Container = ({
 }) => {
   return (
     <div
-      className={cn("container max-w-7xl relative mx-auto px-4 ", className)}
+      className={cn(
+        "container max-w-7xl relative mx-auto px-4 sm:px-6 lg:px-8",
+        className
+      )}
     >
       {children}
     </div>
