f319fecca6037a439a07e216325c2cee5e3ffcc8
rahulcoder2 <rahulchaudharycsit077@indrenicollege.edu.np>
2025-03-24 17:43:17 +0545

    fix: redux store, add: redux-persist ,redux-logger

--- client/src/redux/redux-provider.tsx
+++ client/src/redux/redux-provider.tsx
@@ -1,12 +1,15 @@
 "use client";
 import { Provider } from "react-redux";
-import { store } from "./store";
+import { persistor, store } from "./store";
+import { PersistGate } from "redux-persist/integration/react";
 
 const ReduxProvider = ({ children }: { children: React.ReactNode }) => {
   return (
-    <>
-      <Provider store={store}>{children}</Provider>
-    </>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        {children}
+      </PersistGate>
+    </Provider>
   );
 };
 
--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,12 +1,52 @@
 import { configureStore } from "@reduxjs/toolkit";
-import cartReducer from "./features/cart/cart-slice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { combineReducers } from "redux";
+import {
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist/es/constants";
 
+import logger from "redux-logger";
+import CartReducer from "./features/cart/cart-slice";
+
+const rootReducer = combineReducers({
+  // Add reducers here
+  cart: CartReducer
+});
+
+// Configure Redux Persist
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Create Redux Store
 export const store = configureStore({
-  reducer: {
-    cart: cartReducer,
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => {
+    const middlewareList = getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    });
+
+    if (process.env.NODE_ENV !== "production") {
+      middlewareList.push(logger);
+    }
+
+    return middlewareList;
   },
 });
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
+export const persistor = persistStore(store);
+
+// Types for TypeScript
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
